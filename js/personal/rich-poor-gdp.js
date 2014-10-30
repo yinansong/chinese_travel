6328f5b99f340f8692400b70a5e8e8e67e6db65e
yinansong <yinan@yinansong.com>
2014-10-30 03:49:10 -0400

    clean up the code

---
+++ js/personal/rich-poor-gdp.js
@@ -0,0 +1,100 @@
+$(document).ready(function(){
+    $('#sweden').mouseover(function() {
+        $(this).attr('src', 'img/sweden-b.gif');
+        $(this).css('height', '148px');
+        $(this).css('top', '275px');
+        $(this).css('left', '257px');
+    });
+
+    $('#sweden').mouseout(function() {
+        $(this).attr('src', 'img/sweden-a.gif');
+        $(this).css('height', '93px');
+        $(this).css('top', '300px');
+        $(this).css('left', '282px');
+    });
+
+    $('#finland').mouseover(function() {
+        $(this).attr('src', 'img/finland-b.gif');
+        $(this).css('height', '121px');
+        $(this).css('top', '35px');
+        $(this).css('left', '203px');
+    });
+
+    $('#finland').mouseout(function() {
+        $(this).attr('src', 'img/finland-a.gif');
+        $(this).css('height', '93px');
+        $(this).css('top', '50px');
+        $(this).css('left', '218px');
+    });
+
+    $('#germany').mouseover(function() {
+        $(this).attr('src', 'img/germany-b.gif');
+        $(this).css('height', '112px');
+        $(this).css('top', '190px');
+        $(this).css('left', '150px');
+    });
+
+    $('#germany').mouseout(function() {
+        $(this).attr('src', 'img/germany-a.gif');
+        $(this).css('height', '93px');
+        $(this).css('top', '200px');
+        $(this).css('left', '160px');
+    });
+
+    $('#usa').mouseover(function() {
+        $(this).attr('src', 'img/usa-b.gif');
+        $(this).css('height', '138px');
+        $(this).css('top', '-15px');
+        $(this).css('left', '325px');
+    });
+
+    $('#usa').mouseout(function() {
+        $(this).attr('src', 'img/usa-a.gif');
+        $(this).css('height', '93px');
+        $(this).css('top', '10px');
+        $(this).css('left', '350px');
+    });
+
+    $('#luxembourg').mouseover(function() {
+        $(this).attr('src', 'img/luxembourg-b.gif');
+        $(this).css('height', '277px');
+        $(this).css('top', '160px');
+        $(this).css('left', '360px');
+    });
+
+    $('#luxembourg').mouseout(function() {
+        $(this).attr('src', 'img/luxembourg-a.gif');
+        $(this).css('height', '93px');
+        $(this).css('top', '250px');
+        $(this).css('left', '450px');
+    });
+
+    $('#uk').mouseover(function() {
+        $(this).attr('src', 'img/uk-b.gif');
+        $(this).css('height', '104px');
+        $(this).css('top', '45px');
+        $(this).css('left', '545px');
+    });
+
+    $('#uk').mouseout(function() {
+        $(this).attr('src', 'img/uk-a.gif');
+        $(this).css('height', '93px');
+        $(this).css('top', '50px');
+        $(this).css('left', '550px');
+    });
+
+    $('#denmark').mouseover(function() {
+        $(this).attr('src', 'img/denmark-b.gif');
+        $(this).css('height', '151px');
+        $(this).css('top', '-20px');
+        $(this).css('left', '685px');
+    });
+
+    $('#denmark').mouseout(function() {
+        $(this).attr('src', 'img/denmark-a.gif');
+        $(this).css('height', '93px');
+        $(this).css('top', '10px');
+        $(this).css('left', '715px');
+    });
+
+});
